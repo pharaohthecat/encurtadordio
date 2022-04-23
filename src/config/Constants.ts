6d653630d70fcf4fc5ae83fddc38ef46f81e79e8
Pharaoh <79993523+pharaohthecat@users.noreply.github.com>
2022-04-23 18:01:22 -0300

    Unique Commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+    'mongodb+srv://deyvidsantos:atlas40028922@cluster0.1ly7f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
